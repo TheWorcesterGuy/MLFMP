c75a42587f06a5c6cace328ca1b938d809842463
Francois <chabannes.francois@gmail.com>
2021-11-30 23:14:59 +0100

    Drop duplicated tweets

--- encode_tweets.py
+++ encode_tweets.py
@@ -50,6 +50,11 @@ def main():
 
             enc_df['Datetime'] = pd.to_datetime(enc_df['Datetime'], errors='coerce')
             enc_df['number_cashtag'] = enc_df['Text'].str.count('$')
+            
+            # remove duplicated tweets
+            enc_df['Date'] = pd.to_datetime(enc_df['Datetime'].dt.date)
+            enc_df = enc_df.drop_duplicates(subset=['Text', 'Date'])
+            enc_df = enc_df.drop(['Text', 'Date'], axis=1)
 
             enc_df['Cleaned_Text'] = enc_df.apply(lambda row: clean_text(row), axis=1)
             enc_df['Cleaned_Text'] = enc_df['Cleaned_Text'].astype(str)
