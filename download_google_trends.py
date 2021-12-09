9540bc1ec7d6daddea6dbe3077128d7435bcd4de
Francois <chabannes.francois@gmail.com>
2021-12-09 19:38:50 +0100

    Allowing google trends run at 8:10

--- download_google_trends.py
+++ download_google_trends.py
@@ -269,7 +269,7 @@ def check_update_validity():
         hour = pd.Timestamp.now(tz='US/Eastern').hour
         minute = pd.Timestamp.now(tz='US/Eastern').minute
 
-        if (hour == 8 and 30 <= minute <= 50) is False:
+        if (hour == 8 and 10 <= minute <= 50) is False:
             print('Downloading of Google Trends forbidden : must occur between 8:30am and 8:50am NY time only.')
             exit(0)
         else:
